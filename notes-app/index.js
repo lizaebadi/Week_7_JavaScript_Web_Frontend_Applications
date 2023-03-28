b326b16770b5a2823a4eb22a550a251f06f825b5
lizaebadi <liza-1@hotmail.co.uk>
2023-03-28 12:08:50 +0100

    Model class required in the main file so setup of simple model class is complete

--- notes-app/index.js
+++ notes-app/index.js
@@ -1,2 +1,8 @@
+const NotesModel = require('./notesModel');
+
 console.log("The notes app is running");
 
+const model = new NotesModel();
+model.addNote('Buy milk');
+model.addNote('Buy apples');
+console.log(model.getNotes());
